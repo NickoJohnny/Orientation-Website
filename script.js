2efd5b5dd181516c859ffc8e39608e7be99ce998
Gary Beale <gazzman18@gmail.com>
2021-05-31 15:16:35 +1000

    Minor changes

--- script.js
+++ script.js
@@ -123,3 +123,17 @@ function checkboxes1(){
 		}
 	
 }
+
+// More info button
+function expand() {
+	var x = document.getElementById("pop-up");
+	  if (x.style.display === "none") {
+	    x.style.display = "block";
+	  } else {
+	    x.style.display = "none";
+	  }
+}
+
+function closepop() {
+	document.getElementById("pop-up").style.display = "none";
+}
